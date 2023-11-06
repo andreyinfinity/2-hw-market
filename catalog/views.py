91908b8dd736f329060b822ee52e17b47c19b2d2
Andrey Potapov <aapot@ya.ru>
2023-11-06 18:16:49 +0300

    app: catalog настройка

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request=request, template_name='catalog/index.html')
+
+
+def contacts():
+    pass
